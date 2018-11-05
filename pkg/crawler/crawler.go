5958709f72caccdcf9d881cfa3ae4d5de06f7bf9
David Lawes <davidlawes@hotmail.co.uk>
2018-11-05 14:12:51 +0000

    refactor: extracted retrieval of links to separate function

--- pkg/crawler/crawler.go
+++ pkg/crawler/crawler.go
@@ -2,7 +2,7 @@ package crawler
 
 import (
   "net/http"
-  "sync"
+  "fmt"
   "web-crawler-in-go/pkg/getBody"
   "web-crawler-in-go/pkg/hrefExtractor"
 )
@@ -18,32 +18,46 @@ func Crawl(seedUrl string, client HttpClient) (urlMap UrlMap) {
   urlQueue := make(chan string)
   chFinished := make(chan bool)
 
-  var waitgroup sync.WaitGroup
-  waitgroup.Add(1)
-
+  pChFinished := &chFinished
 
   go func() { urlQueue <- seedUrl }()
 
-  go func() {
-    defer waitgroup.Done()
-    for current_seed := range urlQueue {
-      body := getBody.GetBody(client, current_seed)
-      links := hrefExtractor.Extract(body)
-      urlMap[current_seed] = links
-      chFinished <- true
+  // go func() {
+  //   // for current_seed := range urlQueue {
+  //   //   body := getBody.GetBody(client, current_seed)
+  //   //   links := hrefExtractor.Extract(body)
+  //   //   urlMap[current_seed] = links
+  //   //   chFinished <- true
+  //   // }
+  //   getLinks(urlQueue, client, urlMap, chFinished)
+  // }()
+  for i := 0; i < 1; i++ {
+    select {
+    case url := <- urlQueue:
+      fmt.Println("url added to queue")
+      fmt.Println(url)
+      getLinks(url, client, urlMap, pChFinished)
+    case <- chFinished:
+      fmt.Println("chFinished")
+      i++
+      // break
     }
-  }()
-
-  select {
-  // case url := <- urlQueue:
-  //   fmt.Println("url added to queue")
-  //   fmt.Println(url)
-  case <- chFinished:
-    break
   }
-
-  waitgroup.Wait()
   return
+}
+
+func getLinks(url string, client HttpClient, urlMap UrlMap, pChFinished *chan bool) {
+  fmt.Println("getLinks")
+  fmt.Println(url)
+  // for current_seed := range urlQueue {
+    body := getBody.GetBody(client, url)
+    fmt.Println(body)
+    links := hrefExtractor.Extract(body)
+    fmt.Println(links)
+    urlMap[url] = links
+    *pChFinished <- true
+
+  // }
 }
 
 func addToMap(urlMap UrlMap, links []string) {
