e12451eaf0d97048c0a72e0d33ab5da886150e4f
David Lawes <davidlawes@hotmail.co.uk>
2018-11-11 18:11:40 +0000

    feature: print out to .txt file

--- cmd/webcrawler/main.go
+++ cmd/webcrawler/main.go
@@ -3,12 +3,19 @@ package main
 import (
   "net/http"
   "fmt"
+  "os"
   "github.com/DaveLawes/web-crawler-in-go/pkg/crawler"
+  "github.com/DaveLawes/web-crawler-in-go/pkg/urlPrinter"
 )
 
 func main() {
+  fmt.Println("crawling in progress...")
   seedUrl := "https://monzo.com"
   client := &http.Client{}
   result := crawler.Crawl(seedUrl, client)
-  fmt.Println(result)
+  stringToWrite := urlPrinter.Print(result, seedUrl)
+  text_file, _ := os.Create("Site map.txt")
+  defer text_file.Close()
+  text_file.WriteString(stringToWrite)
+  fmt.Println("Finished! See 'Site map.txt' for result")
 }
